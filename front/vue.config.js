e5e6fb46f8aa742b5b62a12d24f4944750a6ce6a
Xone46 <achraflahcen46@gmail.com>
2025-03-21 14:58:45 +0000

    Finish 2|21|03|2025

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          icon: 'public/logogth.ico' // Pour Windows
+        }
+      }
+    }
+  }
 })
